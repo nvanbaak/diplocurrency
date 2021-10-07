96f62db8e6c3e71a1f3331ece3ff0c65659fc0cb
Nikolai <nvanbaak@gmail.com>
2021-10-07 13:15:17 -0700

    added command to set a country as the bank

---
+++ commands/set-banker.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const account = require('../models/account');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('set-as-bank')
+        .setDescription('designate a country as the Bank')
+        .addRoleOption(option => 
+            option.setName('country')
+                .setDescription('the country to designate')
+                .setRequired(true)),
+    async execute(interaction, auth, { Accounts }) {
+
+        // reprimand non-admin users for their hubris
+        if (!auth.isAdmin) {
+            return interaction.reply( {content: "You're not authorized to use this command.", ephemeral: true} );
+        }
+
+        const targetAccountId = interaction.options._hoistedOptions[0].value;
+        const targetAccountName = interaction.options._hoistedOptions[0].name;
+
+        const accountUpdate = await Accounts.update( {isBank: true, balance: 10000 }, { where: {accountId: targetAccountId} } );
+
+        return interaction.reply(`Set ${targetAccountName} as banker!`);
+    }
+}
